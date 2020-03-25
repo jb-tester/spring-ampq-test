894fb5e43bcb8b9193de19a3999e42d840c6a406
irina_ubuntu <irina@me.com>
2020-03-25 20:20:34 +0300

    xml config

---
+++ src/main/java/com/mytests/spring/ampq/springampqtest/MyListener.java
@@ -0,0 +1,9 @@
+package com.mytests.spring.ampq.springampqtest;
+
+public class MyListener {
+    public void handle(Object o) {
+        System.out.println("!!!!!!!!!!!!!!!!! handled message "+o);
+    }
+
+
+}
--- src/main/java/com/mytests/spring/ampq/springampqtest/SpringAmpqTestApplication.java
+++ src/main/java/com/mytests/spring/ampq/springampqtest/SpringAmpqTestApplication.java
@@ -1,13 +1,23 @@
 package com.mytests.spring.ampq.springampqtest;
 
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Collection;
 
 @SpringBootApplication
 @EnableScheduling
-public class SpringAmpqTestApplication {
-
+@ImportResource("classpath:/rabbit-config.xml")
+public class SpringAmpqTestApplication implements CommandLineRunner {
+   @Autowired
+    ApplicationContext ctx;
     public static void main(String[] args) {
         SpringApplication.run(SpringAmpqTestApplication.class, args);
     }
