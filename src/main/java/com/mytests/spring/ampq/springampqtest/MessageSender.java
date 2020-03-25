409db1ae2910338a9d3f92ea56919b4d5a7164ff
irina_ubuntu <irina@me.com>
2020-03-25 20:26:18 +0300

    send all messages

--- src/main/java/com/mytests/spring/ampq/springampqtest/MessageSender.java
+++ src/main/java/com/mytests/spring/ampq/springampqtest/MessageSender.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import static com.mytests.spring.ampq.springampqtest.MyService.Q6;
+
 /**
  * *******************************
  * Created by irina on 2/18/2020.
@@ -24,9 +26,16 @@ public class MessageSender {
         String message = "message1";
          // some navigating to the matching bindings and queues could be provided?
          // some completion for routing keys?...
-        rabbitTemplate.convertAndSend("my.topic1","foo.bar" , message);
-        rabbitTemplate.convertAndSend("my.topic2","bar.bar" , message);
-        rabbitTemplate.convertAndSend("my.topic2","bar.foo" , message);
+        //rabbitTemplate.convertAndSend("my.topic1","foo.bar" , message);
+        //rabbitTemplate.convertAndSend("my.topic1","xml" , message);
+        //rabbitTemplate.convertAndSend("my.topic2","bar.bar" , message);
+        //rabbitTemplate.convertAndSend("my.topic2","bar.foo" , message);
+        //rabbitTemplate.convertAndSend("my.topic0","q" , message);
+        //rabbitTemplate.convertAndSend("my.topic4","q4" , message);
+        //rabbitTemplate.convertAndSend("my.topic5","q5" , message);
+        rabbitTemplate.convertAndSend("spel.topic","xxx" , message);
+        rabbitTemplate.convertAndSend("spel.topic","yyy" , message);
+        //rabbitTemplate.convertAndSend("",Q6 , message);
        // rabbitTemplate.convertAndSend("myAdminExchange","boo.dummy" , message);
         
     }
